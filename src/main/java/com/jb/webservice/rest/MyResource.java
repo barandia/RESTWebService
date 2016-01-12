2f445f0c7e798915c3cab12f4121f5ba63278fed
Benjie Arandia <arandia.benjie@gmail.com>
2016-01-11 22:17:55 -0500

    Working web service.

---
+++ src/main/java/com/jb/webservice/rest/MyResource.java
@@ -0,0 +1,25 @@
+package com.jb.webservice.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ *
+ * @author benjiearandia
+ */
+@Path("myresource")
+public class MyResource {
+    
+    /**
+     * Method handling HTTP GET requests. The returned object will be sent to 
+     * the client as "text/plain" media type.
+     * @return 
+     */
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
+    }
+}
